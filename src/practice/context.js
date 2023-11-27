36088e072870d75925da367db9b47f1da5f690b0
imwisdom <kjihye0340@naver.com>
2023-11-27 16:50:54 +0900

    useContext hook 사용

--- src/practice/context.js
+++ src/practice/context.js
@@ -1,33 +1,38 @@
-import React, { createContext } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 const UserContext = createContext('unknown');
 function Context() {
-  
+
+  const [name, setName] = useState('mike');
+
   return (
     <div>
-      <UserContext.Provider value="mike">
+      <UserContext.Provider value={name}>
         <div>상단 메뉴</div>
-        <Profile username="mike" />
+        <Profile />
         <div>하단 메뉴</div>
+        <input 
+            type="text"
+            value={name}
+            onChange={e => setName(e.target.value)}
+            />
       </UserContext.Provider>
     </div>
   );
 }
 
-function Profile({username}){
+const Profile = React.memo(function () {
   return (
     <div>
       <Greeting />
     </div>
-  )
-}
+  );
+});
 
-function Greeting({username}){
-  return (
-    <UserContext.Consumer>
-      {username => <p>{`${username}님 안녕하세요`}</p>}
-    </UserContext.Consumer>
-  )
+function Greeting(){
+  const username = useContext(UserContext);
+  return <p>{`${username}님 안녕하세요`}</p>;
+  
 }
 
 export default Context;
