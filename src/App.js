13e2e19dbb1460a82eccb544f2df8e9ec182fba6
imwisdom <kjihye0340@naver.com>
2023-11-27 16:40:08 +0900

    feature: context 실습

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React, { createContext } from 'react';
+import Context from './practice/context';
 
 const UserContext = createContext('unknown');
 function App() {
   
   return (
-    <div>
-      <UserContext.Provider value="mike">
-        <div>상단 메뉴</div>
-        <Profile username="mike" />
-        <div>하단 메뉴</div>
-      </UserContext.Provider>
-    </div>
+    <Context />
   );
-}
-
-function Profile({username}){
-  return (
-    <div>
-      <Greeting />
-    </div>
-  )
-}
-
-function Greeting({username}){
-  return (
-    <UserContext.Consumer>
-      {username => <p>{`${username}님 안녕하세요`}</p>}
-    </UserContext.Consumer>
-  )
 }
 
 export default App;
