2b394dc2f17b4c5619faf914ad4d174f6d6b3daf
imwisdom <kjihye0340@naver.com>
2023-12-04 13:32:43 +0900

    react-redux 사용

--- src/practice/redux/friend/FriendMain.js
+++ src/practice/redux/friend/FriendMain.js
@@ -3,29 +3,16 @@ import store from "../common/store";
 import { addFriend } from "./state";
 import { getNextFriend } from "../common/mockData";
 import FriendList from "./FriendList";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function FriendMain(){
-    const [, forceUpdate] = useReducer(v => v+1, 0);
-    useEffect(() => {
-        //액션이 처리될때마다 무조건 컴포넌트를 업데이트하기때문에 timeline버튼클릭시에도 렌더링
-        //이를 막기 위해 이전 상태값과 현재 상태값 비교
-        let prevFriends = store.getState().friend.friends;
-        const unsubscribe = store.subscribe(() => {
-            const friends = store.getState().friend.friends;
-            if(prevFriends !== friends){
-                forceUpdate();
-            }
-            prevFriends = friends;
-        });
-        return () => unsubscribe();
-    }, []);
-
+    const friends = useSelector(state => state.friend.friends);
+    const dispatch = useDispatch();
     function onAdd(){
         const friend = getNextFriend();
-        store.dispatch(addFriend(friend));
+        dispatch(addFriend(friend));
     }
     console.log('FriendMain render');
-    const friends = store.getState().friend.friends;
     return (
         <div>
             <button onClick={onAdd}>친구 추가</button>
--- src/practice/redux/reduxApp2.js
+++ src/practice/redux/reduxApp2.js
@@ -1,11 +1,15 @@
 import FriendMain from "./friend/FriendMain";
 import TimelineMain from "./timeline/TimelineMain";
+import { Provider } from 'react-redux';
+import store from './common/store';
 
 export default function ReduxApp2(){
     return (
-        <div>
-            <FriendMain />
-            <TimelineMain />
-        </div>
+        <Provider store={store}>
+            <div>
+                <FriendMain />
+                <TimelineMain />
+            </div>
+        </Provider>
     )
 }
